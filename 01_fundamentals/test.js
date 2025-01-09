ae70462c7760427d0b63b26100db6422254bb181
rubab783 <rubab0592@gmail.com>
2025-01-09 08:49:25 +0000

    done

--- 01_fundamentals/test.js
+++ 01_fundamentals/test.js
@@ -1,1 +1,7 @@
 console.log("Node.js");
+let mydate=new Date();
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleString())
+let createdate =new Date(2024,3,8,5,3);
+console.log(createdate.toLocaleString());
